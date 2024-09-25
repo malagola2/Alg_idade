523556b575ab939cfd22fdeb17c842d4f8440aa1
RAFAEL SARAGOCA MALAGOLA <rafael.smalagola@senacsp.edu.br>
2024-09-25 14:33:03 -0300

    Algoritimo de verificação de idade

---
+++ script.js
@@ -0,0 +1,9 @@
+// Solicitar a idade do usuário
+
+let idade = parseInt(prompt("Insira sua idade"));
+
+// Verificar se a idade é maior que 18
+
+if (idade >= 18) {
+    alert ("Acesso permitido! ");
+}else{alert("Acesso não permitido")}
